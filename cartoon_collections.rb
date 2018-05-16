6800ba1d000fb5e2886e8dd9fec97aac27245a90
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-16 00:00:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  tracked = ""
+  array.each_with_index { |dwarf, num|
+    tracked << "#{num + 1}.*#{dwarf}"
+  }
+  puts tracked
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map { |call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(lines)
+  lines.any? { |line| line.size > 4}
 end
 
 def find_the_cheese# code an argument here
